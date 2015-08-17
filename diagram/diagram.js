28f9cd6990085b868290342ff89b77f5983645c7
Guilherme Polo <hello@ggpolo.com>
2015-08-17 03:08:52 -0500

    moving to programmable diagrams

---
+++ diagram/diagram.js
@@ -0,0 +1,285 @@
+var diagram = {};
+
+diagram.cellStyle = {
+  rect: {
+    stroke: '#cccccc',
+    'stroke-width': 2,
+    rx: 3
+  },
+  strokeLink: 'black',
+  reactComponent: {
+    fill: '#53DD61'
+  },
+  reactStore: {
+    fill: '#EDDBF6'
+  },
+  reactAction: {
+    fill: '#7ACFDC'
+  },
+
+  text: {
+    fill: '#000000',
+    'font-size': 13,
+    'font-family': "'Helvetica Neue', helvetica",
+    'font-weight': '500'
+  },
+  noLink: '#444444',
+
+  size: {width: 110, height: 55}
+};
+
+diagram.textStyle = {
+  rect: {
+    'stroke-width': 0,
+    rx: 0
+  },
+  text: {
+    'ref-x': 0,
+    'ref-y': 0,
+    'x-alignment': 'left',
+    'y-alignment': 'top',
+    'font-size': 16,
+    'font-weight': 400
+  }
+};
+
+diagram.linkStyle = {
+  connector: {name: 'rounded'},
+  connection: {
+    stroke: '#333333',
+    'stroke-width': 1.25
+  },
+  markerTarget: {
+    fill: '#333333',
+    d: 'M 6 0 L 0 3 L 6 6 z'
+  }
+};
+
+diagram.regionStyle = {
+  fill: 'white',
+  rx: 0,
+  'stroke-width': 1,
+  stroke: '#cccccc'
+};
+
+diagram.id = 0;
+diagram.elements = {};
+diagram.cells = [];
+diagram.toBack = 0;
+
+
+diagram.clearTemp = function() {
+  var newCells = [];
+
+  /* Remove all links and text elements. */
+  this.cells.forEach(function(cell) {
+    if (cell.type === 'link' ||
+        !cell.attrs.rect['stroke-width']) {
+      return;
+    }
+    newCells.push(cell);
+  });
+
+  this.cells = newCells;
+};
+
+
+diagram.rect = function(name, params) {
+  if (this.elements[name]) {
+    return this.elements[name];
+  }
+
+  this.id++;
+
+  var element = {
+    id: this.id.toString(),
+    type: 'basic.Rect',
+    attrs: {
+      text: $.extend({}, this.cellStyle.text, params.textStyle),
+      rect: $.extend({}, this.cellStyle.rect, params.style)
+    },
+    position: params.position,
+    size: params.size || this.cellStyle.size
+  };
+
+  element.attrs.text.text = params.text;
+
+  if (params.linkTo) {
+    element.prop = {linkTo: params.linkTo};
+    element.attrs.rect.stroke = this.cellStyle.strokeLink;
+  } else {
+    element.attrs.text.fill = this.cellStyle.noLink;
+  }
+
+  this.elements[name] = element;
+
+  return element;
+};
+
+
+diagram.link = function(source, target, displayHead) {
+  this.id++;
+
+  var attrs = {'.connection': this.linkStyle.connection};
+  if (displayHead) {
+    attrs['.marker-target'] = this.linkStyle.markerTarget;
+  }
+
+  return {
+    id: this.id.toString(),
+    type: 'link',
+    source: {id: this.elements[source].id},
+    target: {id: this.elements[target].id},
+    connector: this.linkStyle.connector,
+    router: {name: 'manhattan'},
+    attrs: attrs
+  };
+};
+
+
+diagram._parseName = function(name) {
+  if (typeof name === 'string') {
+    return {raw: name};
+  }
+
+  var res = {
+    raw: name.name,
+    pretty: name.pretty || name.name,
+    style: name.style || {},
+    linkTo: name.link,
+    hide: name.hide,
+    size: name.size,
+    textStyle: name.textStyle
+  };
+  return res;
+};
+
+
+diagram.createLink = function(source, target, displayHead) {
+  this.cells.push(this.link(source, target, displayHead));
+};
+
+
+diagram.createElement = function(name, style, calcPosition) {
+  var res = this._parseName(name);
+  var element = this.rect(res.raw, {
+    linkTo: res.linkTo,
+    text: res.pretty,
+    style: $.extend({}, style, res.style),
+    textStyle: res.textStyle,
+    size: res.size,
+    position: calcPosition()
+  });
+
+  element.attrs.rect.display = res.hide ? 'none' : 'block';
+  element.attrs.text.display = res.hide ? 'none' : 'block';
+  this.cells.push(element);
+  return element;
+};
+
+
+diagram.createText = function(text, position, size, style) {
+  if (!text) {
+    return null;
+  }
+
+  var element = this.createElement({
+    name: joint.util.uuid(),
+    pretty: text,
+    size: size,
+    textStyle: $.extend({}, this.textStyle.text, style)
+  }, this.textStyle.rect, function() { return position; });
+
+  element.z = -1;
+  return element;
+};
+
+
+diagram.nextHorizontal = function(startX, x, y, width) {
+  var inc = this.cellStyle.size.width + 20;
+  var newX = x + inc;
+  var beforeX = newX;
+  var newY = y;
+
+  if (newX + inc >= width) {
+    /* Start a new row. */
+    newX = startX;
+    newY += this.cellStyle.size.height + 20;
+  }
+
+  return {x: newX, y: newY, beforeX: beforeX};
+};
+
+
+diagram.createHorizontalElements = function(pos, elems, style) {
+  var width = pos.width;
+  var maxX = 0;
+  var currPos = {x: pos.x, y: pos.y};
+  var prevPos = {x: pos.x, y: pos.y};
+
+  var self = this;
+  elems.forEach(function(name) {
+    if (name.linebreak) {
+      /* Force a line break. */
+      if (currPos.x !== pos.x) {
+        currPos.x = pos.x;
+        currPos.y += self.cellStyle.size.height + 20;
+      }
+      return;
+    } else if (name.skip) {
+      /* Skip one element. */
+      currPos.x += self.cellStyle.size.width + 20;
+      return;
+    }
+
+    self.createElement(name, style, function() {
+      var position = currPos;
+      prevPos = currPos;
+      currPos = self.nextHorizontal(pos.x, currPos.x, currPos.y, width);
+
+      if (currPos.beforeX > maxX) {
+        maxX = currPos.beforeX;
+      }
+
+      return position;
+    });
+  });
+
+  var bbox = {
+    x1: pos.x - 10, y1: pos.y - 10,
+    x2: maxX - 10, y2: prevPos.y + this.cellStyle.size.height + 10
+  };
+  return bbox;
+};
+
+
+diagram.createParent = function(bbox, name, names, style) {
+  this.toBack--;
+
+  var style = $.extend({}, this.regionStyle, style || {});
+  var region = this.rect(name, {
+    text: '',
+    style: style,
+    position: {x: bbox.x1, y: bbox.y1},
+    size: {
+      width: bbox.x2 - bbox.x1,
+      height: bbox.y2 - bbox.y1
+    }
+  });
+
+  region.embeds = [];
+  region.z = this.toBack;
+
+  names.forEach(function(name) {
+    if (name.linebreak || name.skip) {
+      /* A meta-element. */
+      return;
+    }
+    var raw = this._parseName(name).raw;
+    region.embeds.push(this.elements[raw].id);
+    this.elements[raw].parent = region.id;
+  }, this);
+
+  this.cells.push(region);
+  return region;
+};
---
+++ diagram/render.js
@@ -0,0 +1,182 @@
+render = {};
+
+
+render.links = function(links) {
+  links.forEach(function(link) {
+    if ($.isArray(link)) {
+      diagram.createLink(link[0], link[1]);
+    } else {
+      diagram.createLink(link.source, link.target, link.head);
+    }
+  });
+};
+
+
+render.texts = function(texts) {
+  texts.forEach(function(text) {
+    var placement = text.placement;
+    var position = text.position;
+
+    if (placement && !position) {
+      var reference = diagram.elements[placement[0]];
+      var where = placement[1];
+      if (reference) {
+        position = {
+          x: reference.position.x + (where.right ? reference.size.width * where.right : 20),
+          y: reference.position.y + (where.below ? reference.size.height * where.below : 10)
+        };
+      }
+    }
+
+    diagram.createText(text.content, position, text.size);
+  });
+};
+
+
+render.main = function(info, wrapComponents) {
+
+  wrapComponents = false;  // XXX
+
+  /* Components. */
+  var baseX = 190;
+
+  var startY = 30;
+  diagram.createText(
+    info.components.routed.title,
+    {x: baseX, y: startY}, {width: 600, height: 20});
+
+  var bbox1 = diagram.createHorizontalElements(
+    {x: baseX, y: startY + 30, width: paperSettings.width},
+    info.components.routed.elements,
+    diagram.cellStyle.reactComponent
+  );
+  bbox1.y1 = startY - 10;
+  if (wrapComponents) {
+    diagram.createParent(bbox1, 'routedComponents', info.components.routed.elements);
+  }
+
+  var incY = 30;
+  if (info.components.others.title) {
+    incY = 60;
+    diagram.createText(
+      info.components.others.title,
+      {x: baseX, y: bbox1.y2 + 30}, {width: 600, height: 20});
+  }
+  var bbox2 = diagram.createHorizontalElements(
+    {x: baseX, y: bbox1.y2 + incY, width: paperSettings.width},
+    info.components.others.elements,
+    diagram.cellStyle.reactComponent
+  );
+  bbox2.y1 = bbox1.y2 + 20;
+  if (wrapComponents) {
+    diagram.createParent(bbox2, 'otherComponents', info.components.others.elements);
+  }
+
+  var bbox = {
+    x1: baseX - 20, y1: startY - 20,
+    x2: Math.max(bbox1.x2, bbox2.x2) + 10, y2: bbox2.y2 + 10
+  };
+  if (wrapComponents) {
+    diagram.createParent(
+      bbox, 'components', ['routedComponents', 'otherComponents'],
+      {filter: {name: 'dropShadow', args: {dx: 0, dy: 0, blur: 0.2}}}
+    );
+  }
+
+
+  /* Actions. */
+  diagram.createText(
+    info.actions.title,
+    {x: 15, y: 20}, {width: 130, height: 32},
+    {'font-size': 13}
+  );
+  var x = 20;
+  var y = 60;
+  info.actions.elements.forEach(function(name) {
+    diagram.createElement(name, diagram.cellStyle.reactAction, function() {
+      var position = {x: x, y: y};
+      y += diagram.cellStyle.size.height + 20;
+      return position;
+    })
+  });
+  /*var bbox = {
+    x1: 10, y1: 10,
+    x2: diagram.cellStyle.size.width + 10 + 20, y2: y - 10
+  };
+  diagram.createParent(bbox, 'actions', info.actions.elements);*/
+
+
+  /* Stores. */
+  var y = Math.max(y, bbox.y2 + 20);
+  //msg({x: baseX, y: 665}, info.stores.title, true);
+  diagram.createText(
+    info.stores.title,
+    {x: baseX, y: y + 10}, {width: 600, height: 20},
+    {'font-size': 13}
+  );
+  var bbox = diagram.createHorizontalElements(
+    {x: baseX, y: y + 40, width: paperSettings.width},
+    info.stores.elements,
+    diagram.cellStyle.reactStore
+  );
+  //diagram.createParent(bbox, 'stores', info.stores.elements);
+
+
+  render.links(info.links || []);
+  render.texts(info.texts || []);
+
+  graph.fromJSON({cells: diagram.cells});
+};
+
+
+render.all = function() {
+  render.main(data, true);
+};
+
+
+render._filter = function(candidates, targets) {
+  return candidates.map(function(element) {
+    var inArray = $.inArray(element.name, targets) > -1;
+    return $.extend({}, element, {hide: !inArray});
+  });
+};
+
+
+render.generic = function(entry) {
+  if (entry === 'all') {
+    return render.all();
+  }
+
+  var entryData = data.diagram[entry];
+  if (!entryData.title) {
+    entryData.title = {};
+  }
+  if (!entryData.components) {
+    entryData.components = {};
+  }
+
+  var info = {
+    actions: {
+      title: entryData.title.actions,
+      elements: this._filter(data.actions.elements, entryData.actions)
+    },
+    stores: {
+      title: entryData.title.stores,
+      elements: this._filter(data.stores.elements, entryData.stores)
+    },
+    components: {
+      routed: {
+        title: entryData.title.routed,
+        elements: this._filter(data.components.routed.elements, entryData.components.routed)
+      },
+      others: {
+        title: entryData.title.others,
+        elements: this._filter(data.components.others.elements, entryData.components.others)
+      }
+    },
+    links: entryData.links,
+    texts: entryData.texts
+  };
+
+  this.main(info);
+};
